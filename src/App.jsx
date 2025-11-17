53172363dd45fbe49fd752d4ad661ae9cc450a76
sotogito <sotogitoarchive@gmail.com>
2025-11-17 22:47:31 +0900

    delete : 모닝페이지 about 페이지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import EditorPage from './pages/EditorPage/EditorPage';
 import StatisticsPage from './pages/StatisticsPage/StatisticsPage';
 import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
-import AboutPage from './pages/AboutPage/AboutPage';
 import './styles/global.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/editor" element={<EditorPage />} />
         <Route path="/statistics" element={<StatisticsPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/about" element={<AboutPage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
